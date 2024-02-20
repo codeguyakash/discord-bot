962b0fb12b8befdb23ecaafc9eb47975070e31ef
Akash (codeguyakash) <codeguyakash@gmail.com>
2024-02-20 15:01:44 +0530

    bug fix and add command

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
 import { Client, GatewayIntentBits } from "discord.js";
+import { REST, Routes } from "discord.js";
+import "dotenv/config";
+
+
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+client.on("messageCreate", (message) => {
+  console.log(message.content);
+  if (message.author.bot) return;
+  message.reply({
+    content: "I am BOT",
+  });
+});
+client.on("interactionCreate", (interaction) => {
+  console.log(interaction);
+  interaction.reply("pong");
+});
+
+client.login(process.env.TOKEN);
+// ------------------------------------------------------------------------
+const commands = [
+  {
+    name: "ping",
+    description: "Replies with Pong!",
+  },
+];
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
+
+(async () => {
+  try {
+    console.log("Started refreshing application (/) commands.");
+    await rest.put(Routes.applicationCommands(process.env.CLIENT_ID), {
+      body: commands,
+    });
+    console.log("Successfully reloaded application (/) commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
